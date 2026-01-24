b6232c793c1ec7a741898f3fce8edf6ee6eaaffa
elijah <cuiyeqing960904@gmail.com>
2026-01-24 22:44:06 +0300

    adapter built , building utils - response , env test

--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module dynamo-golang
 go 1.25.5
 
 require (
-	github.com/aws/aws-sdk-go v1.55.8 // indirect
-	github.com/go-chi/chi v1.5.5 // indirect
-	github.com/go-chi/cors v1.2.2 // indirect
+	github.com/aws/aws-sdk-go v1.55.8
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/cors v1.2.2
+	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/repository/adapter/adapter.go
+++ internal/repository/adapter/adapter.go
@@ -1,13 +1,69 @@
 package adapter
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 )
 
 type Database struct {
 	connection *dynamodb.DynamoDB
-	logMode bool
+	logMode    bool
+}
+
+// CreateOrUpdate implements [Interface].
+func (db *Database) CreateOrUpdate(entity interface{}, tableName string) (response *dynamodb.PutItemOutput, err error) {
+	entityParsed, err := dynamodbattribute.MarshalMap(entity)
+	if err != nil {
+		return nil, err
+	}
+	return db.connection.PutItem(&dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item:      entityParsed,
+	})
+}
+
+// Delete implements [Interface].
+func (db *Database) Delete(condition map[string]interface{}, tableName string) (response *dynamodb.DeleteItemOutput, err error) {
+	conditionParsed, err := dynamodbattribute.MarshalMap(condition)
+	if err != nil {
+		return nil, err
+	}
+	return db.connection.DeleteItem(&dynamodb.DeleteItemInput{
+		TableName: aws.String(tableName),
+		Key:       conditionParsed,
+	})
+}
+
+// FindAll implements [Interface].
+func (db *Database) FindAll(condition expression.Expression, tableName string) (response *dynamodb.ScanOutput, err error) {
+	input := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  condition.Names(),
+		ExpressionAttributeValues: condition.Values(),
+		FilterExpression:          condition.Filter(),
+		ProjectionExpression:      condition.Projection(),
+		TableName:                 aws.String(tableName),
+	}
+	return db.connection.Scan(input)
+}
+
+// FindOne implements [Interface].
+func (db *Database) FindOne(condition map[string]interface{}, tableName string) (response *dynamodb.GetItemOutput, err error) {
+	conditionParsed, err := dynamodbattribute.MarshalMap(condition)
+	if err != nil {
+		return nil, err
+	}
+	return db.connection.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key:       conditionParsed,
+	})
+}
+
+// Health implements [Interface].
+func (db *Database) Health() bool {
+	_, err := db.connection.ListTables(&dynamodb.ListTablesInput{})
+	return err == nil
 }
 
 type Interface interface {
@@ -21,6 +77,6 @@ type Interface interface {
 func NewAdapter(con *dynamodb.DynamoDB) Interface {
 	return &Database{
 		connection: con,
-		logMode: false,
+		logMode:    false,
 	}
 }
