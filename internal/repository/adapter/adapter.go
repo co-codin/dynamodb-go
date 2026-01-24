255617314743d9b26e59c3fe6f525030856160ec
elijah <cuiyeqing960904@gmail.com>
2026-01-24 22:15:12 +0300

    connection + building adapter

--- internal/repository/adapter/adapter.go
+++ internal/repository/adapter/adapter.go
@@ -1,1 +1,26 @@
 package adapter
+
+import (
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+)
+
+type Database struct {
+	connection *dynamodb.DynamoDB
+	logMode bool
+}
+
+type Interface interface {
+	Health() bool
+	FindAll(condition expression.Expression, tableName string) (response *dynamodb.ScanOutput, err error)
+	FindOne(condition map[string]interface{}, tableName string) (response *dynamodb.GetItemOutput, err error)
+	CreateOrUpdate(entity interface{}, tableName string) (response *dynamodb.PutItemOutput, err error)
+	Delete(condition map[string]interface{}, tableName string) (response *dynamodb.DeleteItemOutput, err error)
+}
+
+func NewAdapter(con *dynamodb.DynamoDB) Interface {
+	return &Database{
+		connection: con,
+		logMode: false,
+	}
+}
